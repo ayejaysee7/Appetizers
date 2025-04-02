d03feb75c2e575fcb7342395c2b6314172ab8dec
ayejaysee <andrewjchong7@gmail.com>
2025-04-02 12:11:29 -0500

    Added detail views + logic and refactored UI components

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,18 +10,27 @@ import SwiftUI
 struct AppetizerListView: View {
     @StateObject var viewModel = AppetizerListViewModel()
     
+    
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            viewModel.selectedAppetizer = appetizer
+                            viewModel.isShowingDetail = true
+                        }
                 }
                 .navigationTitle("🍟 Appetizers")
                 .listStyle(.plain)
+                .disabled(viewModel.isShowingDetail)
             }.onAppear() {
                 viewModel.getAppetizers()
             }
-            
+            .blur(radius: viewModel.isShowingDetail ? 20 : 0)
+            if viewModel.isShowingDetail {
+                AppetizerDetailView(appetizer: viewModel.selectedAppetizer!, isShowingDetail: $viewModel.isShowingDetail)
+            }
             if viewModel.isLoading {
                 LoadingView()
                 
